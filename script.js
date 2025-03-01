4bb8f8d8cf4f16b5ee96696012e07025be206df0
almogdek <almogdekel7@gmail.com>
2025-03-01 21:56:26 +0200

    .

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 const moviesDiv = document.querySelector('#movies');
-fetch("http://www.omdbapi.com/?t=get+out&apikey=6cf42fd4&")
+
+const apiData = {};
+
+fetch("http://www.omdbapi.com/?s=get+out&apikey=6cf42fd4&")
     .then(response => {
-    if (!response.ok) {
-        throw new Error("No response returned");
-    }
-    else {
-        console.log(response)
+        if (!response.ok) {
+            throw new Error("No response returned (Error: ok: false)");
+        }
         return response.json();
-    }
     })
     .then(data => {
         if (data.Response === 'False') {
-            throw new Error("No response returned");
+            throw new Error("No response returned (Error: Response: false)");
         }
-        else {
-            console.log(data);
-            return data
-        }
+        console.log(data);
+        return data;
     })
     .then(data => {
-        data.Search.forEach(movie => {
-            const movies = document.createElement("div")
-            movies.classList.add("movie");
-            moviesDiv.appendChild(movies);
-            movies.innerHTML = `
+        const movieDetailsPromises = data.Search.map(movie => {
+            return fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=6cf42fd4`)
+                .then(response => response.json())
+        });
+
+        return Promise.all(movieDetailsPromises);
+    })
+    .then(fullMovieDetails => {
+        fullMovieDetails.forEach(movie => {
+            const movieDiv = document.createElement("div");
+            movieDiv.classList.add("movie");
+            moviesDiv.appendChild(movieDiv);
+            movieDiv.innerHTML = `
                 <img src="${movie.Poster}" class="poster" style="max-width: 100%">
                 <div class="movie-description">
-                    <p class="movie-title"> <b class="title">` + movie.Title + `</b> (`+ movie.Year + `)</p>
-                    
-                    <p></p>
-                </div>`
-
-                
-
-        })
+                    <p class="movie-title"><b class="title">${movie.Title}</b> (${movie.Year})</p>
+                    <p>${movie.Plot}</p>
+                </div>`;
+        });
     })
     .catch(error => console.log(error));
